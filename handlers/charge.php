36a9fd2710ca627c19cc6427028627bfa5a47586
jbroadway <johnny@johnnybroadway.com>
2013-05-14 11:54:02 -0500

    /stripe/charge now saves to the payment history as well

--- handlers/charge.php
+++ handlers/charge.php
@@ -8,16 +8,48 @@
  * transaction.
  */
 
+// Verify that they're on an SSL connection
+$this->force_https ();
+
 // Initialize the Stripe API
 $this->run ('stripe/init');
 
 // Perform the charge
-$charge = Stripe_Charge::create (array (
-	'card' => $_POST['stripeToken'],
-	'amount' => $_POST['amount'],
+try {
+	$charge = Stripe_Charge::create (array (
+		'card' => $_POST['stripeToken'],
+		'amount' => $_POST['amount'],
+		'description' => $_POST['description'],
+		'currency' => isset ($_POST['currency']) ? $_POST['currency'] : Appconf::stripe ('Stripe', 'currency')
+	));
+} catch (Stripe_CardError $e) {
+	// The card was declined
+	echo $this->error (, 'Card declined', 'Your card was declined. Please go back and verify your information.');
+	return;
+} catch (Exception $e) {
+	// Handle error
+	error_log ('Error saving stripe_charge: ' . $e->getMessage ();
+	echo $this->error (500, 'An error occurred', 'Unable to save customer info at this time. Please try again later.');
+	return;
+}
+
+// Save the payment
+$p = new stripe\Payment (array (
+	'user_id' => 0,
+	'stripe_id' => $charge->id,
 	'description' => $_POST['description'],
-	'currency' => isset ($_POST['currency']) ? $_POST['currency'] : 'usd'
+	'amount' => $_POST['amount'],
+	'plan' => '',
+	'ts' => gmdate ('Y-m-d H:i:s'),
+	'ip' => ip2long ($_SERVER['REMOTE_ADDR']),
+	'type' => 'payment'
 ));
+if (! $p->put ()) {
+	// Handle error
+	error_log ('Error saving payment to database: ' . $p->error);
+	echo $this->error (500, 'An error occurred', 'Unable to save payment info at this time. Please contact support for assistance.');
+	return;
+}
 
 // Redirect if they've provided one
 if (isset ($_POST['redirect'])) {
