57e2c90ba000e48b230b4939c3c8e2be0d842292
jbroadway <johnny@johnnybroadway.com>
2013-05-21 14:21:12 -0500

    Added webhook callback

--- handlers/webhook.php
+++ handlers/webhook.php
@@ -7,14 +7,29 @@
  * URL (adjusted for your own domain) to your Stripe
  * dashboard:
  *
- *     http://www.example.com/stripe/webhook
+ *     https://www.example.com/stripe/webhook
  */
 
 $page->layout = false;
 
 $this->run ('stripe/init');
 
+// Get data and make sure it's okay
 $data = $this->get_put_data ();
 $event = json_decode ($data);
+if ($event === null) {
+	return;
+}
+
+// Retrieve the original event for added security
+try {
+	$event = Stripe_Event::retrieve ($event->id);
+} catch (Exception $e) {
+	return;
+}
+
+if (! empty ($appconf['Stripe']['webhook_handler'])) {
+	echo $this->run ($appconf['Stripe']['webhook_handler'], $event);
+}
 
 ?>
