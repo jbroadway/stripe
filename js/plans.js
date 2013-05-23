25b9a8d8fbba5cc2da0b7df4fb5f993339fb2d28
jbroadway <johnny@johnnybroadway.com>
2013-05-23 16:24:59 -0500

    Add plan dialog started

--- js/plans.js
+++ js/plans.js
@@ -15,19 +15,34 @@ var plans = (function ($) {
 		self.form = Handlebars.compile ($('#plan-form-template').html ());
 		self.tpl = Handlebars.compile ($('#plan-template').html ());
 
-		$('#add-plan').click (self.add_plan);
+		$('#add-plan').click (self.add_plan_dialog);
 		$('#plan-list').on ('click', '.remove-plan', self.remove_plan);
 		$('#plan-list').on ('blur', 'input[type="text"]', self.update);
 	};
 
+	self.add_plan_dialog = function (event) {
+		event.preventDefault ();
+		$.open_dialog (
+			$.i18n ('Add plan'),
+			self.form ({
+				plan: '',
+				label: '',
+				amount: '',
+				interval: 'monthly'
+			})
+		);
+		$('#plan-submit').click (self.add_plan);
+	};
+	
 	self.add_plan = function (event) {
 		event.preventDefault ();
-		$('#plan-list').append (self.tpl ({
+		/*$('#plan-list').append (self.tpl ({
 			plan: 'basic',
 			label: 'Basic',
 			amount: (1500/100).toFixed (2),
 			interval: 'monthly'
-		}));
+		}));*/
+		$.close_dialog ();
 	};
 
 	self.remove_plan = function (event) {
