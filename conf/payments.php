f5a97651684509c48461540baecbfdf2eff80202
Johnny Broadway <johnny@johnnybroadway.com>
2013-06-20 10:29:00 -0500

    Created conf file to specify supplied payment handlers for other apps

---
+++ conf/payments.php
@@ -0,0 +1,11 @@
+; <?php
+
+; This declares that the stripe/payments handler should be included
+; as "Stripe Payments" in the list of available payment handlers in
+; Elefant. For documentation on the fields expected of a payment
+; handler, see:
+; https://github.com/jbroadway/stripe#creating-a-member-payment-or-subscription-form
+
+stripe/payment = "Stripe Payments"
+
+; */ ?>
