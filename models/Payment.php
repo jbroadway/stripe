734d17c4d068e632808e1c7a33c3df0cb4ff3d39
jbroadway <johnny@johnnybroadway.com>
2014-05-30 13:50:32 -0500

    Updated field list

--- models/Payment.php
+++ models/Payment.php
@@ -14,6 +14,21 @@ namespace stripe;
  * - amount
  * - plan
  * - ts
+ * - ip
+ * - type
+ * - email
+ * - billing_name
+ * - billing_address
+ * - billing_city
+ * - billing_state
+ * - billing_country
+ * - billing_zip
+ * - shipping_name
+ * - shipping_address
+ * - shipping_city
+ * - shipping_state
+ * - shipping_country
+ * - shipping_zip
  */
 class Payment extends \Model {
 	public $table = '#prefix#payment';
