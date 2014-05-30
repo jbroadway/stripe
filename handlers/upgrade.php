3ee4f4b3500b50df8409a7b49510a929658db2c2
jbroadway <johnny@johnnybroadway.com>
2014-05-30 11:52:59 -0500

    Added email and address to payments table

--- handlers/upgrade.php
+++ handlers/upgrade.php
@@ -12,6 +12,36 @@ if ($this->installed ('stripe', $appconf['Admin']['version']) === true) {
 
 $page->title = 'Upgrading app: Stripe Payments';
 
+if ($appconf['Admin']['version'] === '0.5.0-beta') {
+	$updates = array (
+		"alter table #prefix#payment add column email char(72) not null default ''",
+		"alter table #prefix#payment add column billing_name char(72) not null default ''",
+		"alter table #prefix#payment add column billing_address char(72) not null default ''",
+		"alter table #prefix#payment add column billing_address2 char(72) not null default ''",
+		"alter table #prefix#payment add column billing_city char(72) not null default ''",
+		"alter table #prefix#payment add column billing_state char(72) not null default ''",
+		"alter table #prefix#payment add column billing_country char(72) not null default ''",
+		"alter table #prefix#payment add column shipping_name char(72) not null default ''",
+		"alter table #prefix#payment add column shipping_address char(72) not null default ''",
+		"alter table #prefix#payment add column shipping_address2 char(72) not null default ''",
+		"alter table #prefix#payment add column shipping_city char(72) not null default ''",
+		"alter table #prefix#payment add column shipping_state char(72) not null default ''",
+		"alter table #prefix#payment add column shipping_country char(72) not null default ''"
+	);
+
+	DB::beginTransaction ();
+	foreach ($updates as $update) {
+		if (! DB::execute ($update)) {
+			$error = DB::error ();
+			DB::rollback ();
+			printf ('<p class="visible-notice">%s: %s</p>', __ ('Error'), $error);
+			printf ('<p>%s</p>', __ ('Upgrade failed.'));
+			return;
+		}
+	}
+	DB::commit ();
+}
+
 echo '<p>Done.</p>';
 
 $this->mark_installed ('stripe', $appconf['Admin']['version']);
